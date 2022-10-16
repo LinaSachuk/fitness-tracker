f617b9e7b45e7b57bbc926d9b12a569daad0c25d
Lina Sachuk <sachuklina@gmail.com>
2022-10-16 14:47:57 -0600

    added training component

--- angular-fitness/src/app/app.module.ts
+++ angular-fitness/src/app/app.module.ts
@@ -9,6 +9,14 @@ import { ProductsService } from './products/products.service';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +24,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ProductsComponent,
     ProductComponent,
     HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [ProductsService],
   bootstrap: [AppComponent],
 })
