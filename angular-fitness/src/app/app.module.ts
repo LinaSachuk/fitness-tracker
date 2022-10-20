6cc5ff130ef2b93f388df03dcda84c72560936f3
Lina Sachuk <sachuklina@gmail.com>
2022-10-19 21:36:59 -0600

    added signup component

--- angular-fitness/src/app/app.module.ts
+++ angular-fitness/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
@@ -40,6 +43,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent],
